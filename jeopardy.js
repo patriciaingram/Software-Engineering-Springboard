f15f04e7d76bd80819380d5d627778a7ad5dbfb1
pingram <patty.ingram@gmail.com>
2024-03-26 20:39:27 -0400

    Add jeopardy.js

--- jeopardy.js
+++ jeopardy.js
@@ -2,32 +2,9 @@ const BASE_API_URL = "https://rithm-jeopardy.herokuapp.com/api/";
 const NUM_CATEGORIES = 6;
 const NUM_CLUES_PER_CAT = 5;
 
-// categories is the main data structure for the app; it looks like this:
-
-//  [
-//    { title: "Math",
-//      clues: [
-//        {question: "2+2", answer: 4, showing: null},
-//        {question: "1+1", answer: 2, showing: null}
-//        ...
-//      ],
-//    },
-//    { title: "Literature",
-//      clues: [
-//        {question: "Hamlet Author", answer: "Shakespeare", showing: null},
-//        {question: "Bell Jar Author", answer: "Plath", showing: null},
-//        ...
-//      ],
-//    },
-//    ...
-//  ]
-
 let categories = [];
 
-/** Get NUM_CATEGORIES random category from API.
- *
- * Returns array of category ids
- */
+
 
 async function getCategoryIds() {
   // ask for 100 categories [most we can ask for], so we can pick random
@@ -38,17 +15,7 @@ async function getCategoryIds() {
   return _.sampleSize(catIds, NUM_CATEGORIES);
 }
 
-/** Return object with data about a category:
- *
- *  Returns { title: "Math", clues: clue-array }
- *
- * Where clue-array is:
- *   [
- *      {question: "Hamlet Author", answer: "Shakespeare", showing: null, value: 200},
- *      {question: "Bell Jar Author", answer: "Plath", showing: null, value: 400},
- *      ...
- *   ]
- */
+
 
 async function getCategory(catId) {
   let response = await axios.get(`${BASE_API_URL}category`, {
@@ -63,25 +30,17 @@ async function getCategory(catId) {
   return { title: cat.title, clues: randomClues };
 }
 
-/** Fill the HTML table#jeopardy with the categories & cells for questions.
- *
- * - The <thead> should be filled w/a <tr>, and a <td> for each category
- * - The <tbody> should be filled w/NUM_QUESTIONS_PER_CAT <tr>s,
- *   each with a question for each category in a <td>
- *   (initally, just show a "?" where the question/answer would go.)
- */
 
 async function fillTable() {
   hideLoadingView();
 
-  // Add row with headers for categories
+
   let $tr = $("<tr>");
   for (let category of categories) {
     $tr.append($("<th>").text(category.title));
   }
   $("#jeopardy thead").append($tr);
 
-  // Add rows with questions for each category
   $("#jeopardy tbody").empty();
   for (let clueIdx = 0; clueIdx < NUM_CLUES_PER_CAT; clueIdx++) {
     let $tr = $("<tr>");
@@ -96,13 +55,7 @@ async function fillTable() {
   }
 }
 
-/** Handle clicking on a clue: show the question or answer.
- *
- * Uses .showing property on clue to determine what to show:
- * - if currently null, show question & set .showing to "question"
- * - if currently "question", show answer & set .showing to "answer"
- * - if currently "answer", ignore click
- * */
+
 
 function handleClick(evt) {
   let $tgt = $(evt.target);
@@ -120,31 +73,29 @@ function handleClick(evt) {
     clue.showing = "answer";
     $tgt.addClass("disabled");
   } else {
-    // already showing answer; ignore
+    
     return;
   }
 
-  // Update text of cell
+ 
   $tgt.html(msg);
 }
 
-/** Wipe the current Jeopardy board, show the loading spinner,
- * and update the button used to fetch data.
- */
+
 
 function showLoadingView() {
-  // clear the board
+  
   $("#jeopardy thead").empty();
   $("#jeopardy tbody").empty();
 
-  // show the loading icon
+  
   $("#spin-container").show();
   $("#start")
     .addClass("disabled")
     .text("Loading...");
 }
 
-/** Remove the loading spinner and update the button used to fetch data. */
+
 
 function hideLoadingView() {
   $("#start")
@@ -153,12 +104,7 @@ function hideLoadingView() {
   $("#spin-container").hide();
 }
 
-/** Start game:
- *
- * - get random category Ids
- * - get data for each category
- * - create HTML table
- * */
+
 
 async function setupAndStart() {
   let isLoading = $("#start").text() === "Loading...";
